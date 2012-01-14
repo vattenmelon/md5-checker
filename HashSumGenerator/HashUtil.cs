afc57917e9216559ff391fe0e10f3e8c5678a12f
Erling Reizer <reizer@gmail.com>
2012-01-14 22:20:00 +0100

    add files

---
+++ HashSumGenerator/HashUtil.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+using System.Windows.Forms;
+
+namespace HashSumGenerator
+{
+    class HashUtil
+    {
+        public static String ToMd5(String input)
+        {
+            return generateHash(input, System.Security.Cryptography.MD5.Create());
+        }
+
+        public static String ToSha256(String input)
+        { 
+            return generateHash(input, System.Security.Cryptography.SHA256.Create());
+        }
+
+        private static String generateHash(String path, HashAlgorithm algorithm)
+        {
+  
+            FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read);
+
+            byte[] hash = algorithm.ComputeHash(file);
+
+            StringBuilder sbuilder = new StringBuilder();
+            foreach (byte bite in hash)
+            {//"X2"
+                sbuilder.Append(bite.ToString("x2"));
+            }
+            return sbuilder.ToString();
+        }
+    }
+}
---
+++ HashSumGenerator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace HashSumGenerator
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            String path = args[0];
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            MainWindow f = new MainWindow();
+            f.Label_Filename = path.Substring(path.LastIndexOf("\\") + 1);
+            f.TextBox_MD5Text = HashUtil.ToMd5(path);
+            f.TextBox_Sha256Text = HashUtil.ToSha256(path);
+            Application.Run(f);
+        }
+    }
+}
